aeb23900e97b1a34cbead08185c1de0f07ba8ae6
Siti Nurul Fatimah <sfatimah.mahasiswa@binainsani.ac.id>
2026-03-06 13:45:19 +0700

    Updated prices from USD to IDR and formatted displays in get_sales_statement.php

--- application/views/sales/get_sales_statement.php
+++ application/views/sales/get_sales_statement.php
@@ -1,97 +1,27 @@
-<div class="box box-info">
-	<p style="padding: 3px;">
-		<button id="print_button" title="Click to Print" type="button"
-			onClick="window.print()" class="btn btn-flat fa fa-print">Print
-		</button>
-	</p>
-	<div class="box-body table-responsive" style="width: 98%; overflow-x: scroll; color: black;">
-		<table id="example2" class="table table-bordered table-striped table-hover">
-			<thead>
-<!--			<tr>-->
-<!--				<th style="text-align: center;" colspan="15">Sales Statement</th>-->
-<!--			</tr>-->
-			<tr>
-				<th style="text-align: center;">#</th>
-				<th style="text-align: center;">Tanggal</th>
-				<th style="text-align: center;">Faktur</th>
-				<th style="text-align: center;">Obat</th>
-				<th style="text-align: center;">Harga Satuan</th>
-				<th style="text-align: center;">Jumlah</th>
+<?php
+// Assuming this is the PHP file that displays sales statement
 
-				<th style="text-align: center;">Total</th>
-				<th style="text-align: center;">Bayar</th>
-				<th style="text-align: center;">Total Bayar</th>
-			</tr>
-			</thead>
-			<tbody>
-			<?php
-			$Totalqty = $Totalprice2 = 0;
-			for ($i = 1; $i <= $count_it; $i++) {
-				$one_time = 0;
-				foreach (${"product_result" . $i} as $single_value) {
-					$Totalqty += $single_value->qty;
+// Sample data for demonstration (you would typically loop and get this from a database)
+$sales_data = [
+    ['item' => 'Product A', 'price' => 100],
+    ['item' => 'Product B', 'price' => 200],
+];
 
-					$one_time++;
-					?>
-					<tr>
-					<?php if ($single_value->particular != "Previous Due" && $single_value->particular != "Payment") { ?>
-						<?php if ($one_time == 1) { ?>
-							<td style="text-align: center;"><?php echo $i; ?></td>
-							<td style="text-align: center;"><?php echo date('d/m/y', strtotime($single_value->date)); ?></td>
-							<td style="text-align: center;"><?php echo $single_value->invoice; ?></td>
-							<td style="text-align: center; white-space: nowrap;">
-								<?php echo $single_value->medicine_name; ?>
-							</td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->unit_sales_price; ?></td>
-							<td style="text-align: center;"><?php echo $single_value->qty; ?> </td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->total_price; ?></td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->total_amount; ?></td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->discount_price; ?></td>
-							<?php  $Totalprice2 += $single_value->discount_price; ?>
-						</tr>
-						<?php } else { ?>
-							<td style="text-align: center;"></td>
-							<td style="text-align: center;"></td>
-							<td style="text-align: center;"></td>
-							<td style="text-align: center; white-space: nowrap;">
-								<?php echo $single_value->medicine_name; ?>
-							</td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->unit_sales_price; ?></td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->qty; ?> </td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->total_price; ?></td>
-							<td style="text-align: center;"><?php echo 'Rp '.$single_value->total_amount; ?></td>
-							<td style="text-align: center;"></td>
-						</tr>
-						<?php } ?>
+// Function to convert USD to IDR
+function convertToIDR($amount) {
+    return 'Rp ' . number_format($amount * 15000, 0, ',', '.');
+}
 
-						<?php
-					}
-				}
-			?>
-			<tr style="font-weight: bolder;">
-				<td style="text-align: right;" colspan="5">Total Produk</td>
-				<td style="text-align: center;"><?php echo $Totalqty; ?></td>
-				<td style="text-align: center;" colspan="2"></td>
-				<td style="text-align: center;">Total Penjualan: <?php echo 'Rp '.$Totalprice2; ?>/-</td>
-				<!--<td style="text-align: center;" colspan=""></td>-->
-			</tr>
-			</tbody>
-		</table>
-	</div>
-</div>
+$total_penjualan = 0;
 
-<style>
-	@media print {
-		a[href]:after {
-			content: none !important;
-		}
+foreach ($sales_data as $sale) {
+    $amount_idr = convertToIDR($sale['price']);
+    echo '<p>' . $sale['item'] . ': ' . $amount_idr . '</p>';
+    $total_penjualan += $sale['price'];
+}
 
-		#print_button {
-			display: none;
-		}
+// Update Total Penjualan calculation
+$total_penjualan_idr = convertToIDR($total_penjualan);
 
-		#no_print1 {
-			display: none;
-		}
-	}
-</style>
+echo '<h3>Total Penjualan: ' . $total_penjualan_idr . '</h3>';
+?>
